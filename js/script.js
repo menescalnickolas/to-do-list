0a3f16c278ed084e5b37baecff147060f6fca405
menescalnickolas <menescalnickolas@gmail.com>
2024-05-25 14:16:39 +0200

    Attempt to crossOut( ) and add delete button

--- js/script.js
+++ js/script.js
@@ -1,17 +1,31 @@
+
+
 function newItem() {
     /* Add a new item*/
-    
     let inputValue = $('#input').val();
-    let li = $(<li></li>).text(inputValue);
+    let li = $('<li>').text(inputValue);
     $('#list').append(li);
 
     if (inputValue === '') {
         alert("You must write something!");
-      } else {
+    } else {
         $('#list').append(li);
-      }
+    }
+
+    /*Cross Out Item*/
+    function crossOut() {
+        li.addClass('strike');
+    }
+    li.on('dblclick', crossOut);
+
+    /*Delete Button*/
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode("X"));
+    li.append(crossOutButton);
+
+    crossOutButton.on('click', deleteListItem);
 
 
 
 
-}
+} 
